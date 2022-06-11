57501a7199a30335305cb389f7508b38f708bd99
Chielo Newctle <ChieloNewctle@Yandex.com>
2022-06-11 23:34:42 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
 ISL_ROOT="$(dirname "$0")"
+cd "$ISL_ROOT"
 
-if [ ! -e "$ISL_ROOT/.libs/libisl.so" ]; then (
-    echo "Build libisl.so"
-    cd "$ISL_ROOT"
+if [ ! -e "./configure" ]; then (
     autoreconf -i
-    ./configure --with-clang=system
-    make $@
+    ./configure --with-clang=system --with-int=imath
 ) fi
 
-if [ ! -e "$ISL_ROOT/interface/isl.py" ]; then (
-    echo "Build isl.py"
-    cd "$ISL_ROOT"
-    make $@ interface/isl.py
-) fi
+make $@ all interface/isl.py
 
 ln -sf $(realpath $ISL_ROOT/interface/isl.py) $ISL_ROOT/isl.py
 ln -sf $(realpath $ISL_ROOT/.libs/libisl.so) $ISL_ROOT/libisl.so
