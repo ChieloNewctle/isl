abec1a056e8ae84a5b8e23c170f697cf9911189b
Chielo Newctle <ChieloNewctle@Yandex.com>
2023-03-02 00:38:50 +0800

    update build script

--- build.sh
+++ build.sh
@@ -3,9 +3,15 @@
 ISL_ROOT="$(dirname "$0")"
 cd "$ISL_ROOT"
 
+git submodule update --init --recursive
+
 if [ ! -e "./isl_config.h" ]; then
     autoreconf -i
     ./configure --with-clang=system --with-int=imath
+fi
+
+if [ ! -e "./gitversion.h" ]; then
+  make gitversion.h
 fi
 
 make "$@" libisl.la interface/isl.py
